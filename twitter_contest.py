1ce01d7e2e5a92ba64407f149243b9e88df244f8
awsmdavid <awsmdavid@gmail.com>
2015-10-09 11:42:10 -0400

    updated with additional followers

--- twitter_contest.py
+++ twitter_contest.py
@@ -9,12 +9,10 @@ import tweepy
 # For Cronjob Command (if you need virtualenv)
 # source /....../bin/activate && python /..../twitter_contest.py
 
-
-# CONSUMER_KEY = "blah"
-# CONSUMER_SECRET = "blah"
-# ACCESS_TOKEN = "blah"
-# ACCESS_TOKEN_SECRET = "blah"
-
+CONSUMER_KEY = "blah"
+CONSUMER_SECRET = "blah"
+ACCESS_TOKEN = "blah"
+ACCESS_TOKEN_SECRET = "blah"
 
 # OAuth Handshake
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -28,9 +26,12 @@ api = tweepy.API(auth)
 # TODO:
 # Refine query parameters
 # dont reprocess same tweets (perhaps use since_id)
+
+# search_results = api.search(q="retweet follow", rpp=100, lang="en")
 search_results = api.search(q="retweet follow giveaway contest -RT", rpp=100, lang="en")
+
 # 	Parameters:
-# q the search query string
+# q the search query string: for more info - https://twitter.com/search-home (click operators)
 # lang - Restricts tweets to the given language, given by an ISO 639-1 code.
 # locale - Specify the language of the query you are sending. This is intended for language-specific clients and the default should work in the majority of cases.
 # rpp - The number of tweets to return per page, up to a max of 100.
@@ -42,28 +43,57 @@ search_results = api.search(q="retweet follow giveaway contest -RT", rpp=100, la
 # iterate over each search result
 for tweet in search_results:
     tweet_id = tweet.id
+    tweet_text = tweet.text
     # check if the tweet is authentic or a retweet
     # hasattr returns false if the tweet has not been retweeted, i.e., is the original tweet
     if hasattr(tweet, "retweeted_status"):
-        # print "nope", "\n", tweet, "\n"
+        # print "yo", tweet_text
         pass
     # check for original tweets
     else:
         # TODO:
         # certain tweets have multiple follow demands, e.g., follow @person1 AND @person2 to be eligible
 
-        # get user screen name
+        list_of_accounts_to_follow = []
+
+        ######################
+        # Retweet and Follow #
+        ######################
+
+        # get user screen name to follow initial tweeter
         user_screen_name_to_follow = api.get_status(id=tweet_id).user.screen_name
-        # follow the user
+        list_of_accounts_to_follow.append(user_screen_name_to_follow)
+
         try:
             # Retweet the contest promo tweet
             retweet_result = api.retweet(id=tweet_id)
-            # print user_screen_name_to_follow
-            Follow_result = api.create_friendship(screen_name=user_screen_name_to_follow)
-            print "yep", "\n", tweet.text
+
+            # Follow mad people
+            text_list = tweet_text.split(" ")
+            for text in text_list:
+                text.strip("\n")
+                text.strip("\t")
+                if text.startswith("@"):
+                    list_of_accounts_to_follow.append(text)
         except:
             pass
 
+        # Follow all accounts mentioned
+        for screen_name in list_of_accounts_to_follow:
+            # Cleanse accounts of "@"
+            screen_name = screen_name.replace("@", "")
+            try:
+                Follow_result = api.create_friendship(screen_name=screen_name)
+            except:
+                pass
+
+
+
+
+
+################################
+# Random Notes for future use: #
+################################
 
 # Tweet Parameters
 # print ("ID:", tweet.id)
@@ -84,9 +114,9 @@ for tweet in search_results:
 #     print ("Retweet count:", tweet.retweet_count)
 #     print ("Source:", tweet.source)
 #     print ("Truncated:", tweet.truncated)
-
-
-# Random Notes for future use:
+#
+#
+#
 # If you see the following warning:
 #   "InsecurePlatformWarning: A true SSLContext object is not available. This prevents urllib3
 #   from configuring SSL appropriately and may cause certain SSL connections to fail."
@@ -94,7 +124,7 @@ for tweet in search_results:
 #
 # Alternatively, if you dont want to upgrade or don't have sudo permission, you can surpress the warnings by downgrading your requests:
 # pip install requests==2.5.3
-
-
+#
+#
 # use this to send tweets - must have header or else will throw an error
 #   Result = api.update_status(status=TweetText, in_reply_to_status_id=ReplyToTweetID, headers={'content-length': 0})
